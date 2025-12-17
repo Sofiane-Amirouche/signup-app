dc5baa81c83de70c549ff1dbb511121239818d1d
Sofiane <sofiane.amiro@gmail.com>
2025-12-17 13:49:12 -0800

    Switch to bcryptjs for Render compatibility

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
+
 const nodemailer = require('nodemailer');
 const path = require('path');
 
